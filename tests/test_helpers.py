0dac6e064e34818e02927872e79bce1efe345d60
Venkat Gorla <venkatagorla@gmail.com>
2025-04-08 11:46:22 +0530

    [unit test refactor] move producer consumer threading logic inside a separate file - tests passing - more to be added

---
+++ tests/test_helpers.py
@@ -0,0 +1,40 @@
+import threading
+import time
+from typing import List
+
+class ProducerConsumerTest:
+    def __init__(
+        self,
+        queue,
+        input_elements: List[int],
+        producer_delay: float = 0.1,
+        consumer_delay: float = 0.2,
+    ):
+        self.queue = queue
+        self.input_elements = input_elements
+        self.results: List[int] = []
+        self.producer_delay = producer_delay
+        self.consumer_delay = consumer_delay
+
+    def producer(self):
+        for item in self.input_elements:
+            self.queue.add_item(item)
+            time.sleep(self.producer_delay)
+
+    def consumer(self):
+        for _ in self.input_elements:
+            item = self.queue.remove_item()
+            self.results.append(item)
+            time.sleep(self.consumer_delay)
+
+    def run(self):
+        producer_thread = threading.Thread(target=self.producer)
+        consumer_thread = threading.Thread(target=self.consumer)
+
+        producer_thread.start()
+        consumer_thread.start()
+
+        producer_thread.join()
+        consumer_thread.join()
+
+        return self.results
--- tests/test_multi_queue.py
+++ tests/test_multi_queue.py
@@ -1,35 +1,14 @@
 import unittest
 from src.multi_queue import MultiQueue
-import threading
-import time
-from typing import List
+from tests.test_helpers import ProducerConsumerTest
 
 class TestMultiQueue(unittest.TestCase):
     def test_single_producer_consumer(self):
         queue = MultiQueue(2)
         input_elements = list(range(5))
-        results: List[int] = []
 
-        # should be packaged inside an object that can be used in multiple tests
-        def producer():
-            for i in input_elements:
-                queue.add_item(i)
-                time.sleep(0.1)  # Simulate processing time
-
-        def consumer():
-            for _ in input_elements:
-                item = queue.remove_item()
-                results.append(item)
-                time.sleep(0.2)  # Simulate processing time
-
-        producer_thread = threading.Thread(target=producer)
-        consumer_thread = threading.Thread(target=consumer)
-
-        producer_thread.start()
-        consumer_thread.start()
-
-        producer_thread.join()
-        consumer_thread.join()
+        test = ProducerConsumerTest(queue, input_elements)
+        results = test.run()
 
         self.assertEqual(results, input_elements)  # Ensure all items are consumed in order
 
